5b79241159eb919256e66bbcaf09c15e38c47535
Angela Dudtkowski <dudtkowski@clickstorm.de>
2017-03-02 16:17:05 +0100

    [BUGFIX] Check for broken extensions failed

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,7 @@
 <?php
 
+$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+
 // Convert images on processing files
 $signalSlotDispatcher->connect(
     'TYPO3\\CMS\\Core\\Resource\\ResourceStorage',
